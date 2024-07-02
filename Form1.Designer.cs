be9065f145da643cdc28d9d8de66f7d23b081a70
fitcvn <trhh@lungtungbl.eu.org>
2024-07-02 21:08:44 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace AOEPatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Estbt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thư mục cài đặt AOE:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chọn thư mục";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Patch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Estbt
+            // 
+            this.Estbt.Location = new System.Drawing.Point(600, 316);
+            this.Estbt.Name = "Estbt";
+            this.Estbt.Size = new System.Drawing.Size(148, 53);
+            this.Estbt.TabIndex = 5;
+            this.Estbt.Text = "Thoát";
+            this.Estbt.UseVisualStyleBackColor = true;
+            this.Estbt.Click += new System.EventHandler(this.Estbt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Estbt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AOE Patch by K81";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Estbt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AOEPatch
+{
+    public partial class Form1 : Form
+    {
+        public int patched = 0;
+        public string aoepath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //folderBrowserDialog1.ShowDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                aoepath = path;
+            }
+            textBox1.Text = aoepath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int ttf = 0;
+            if(File.Exists(aoepath + "\\EMPIRES.EXE"))
+            {
+                ttf += 1;
+            }
+            else
+            {
+                MessageBox.Show("Không phát hiện file game: 'EMPIRES.EXE' tại, vui lòng thử lại");
+            }
+            if (File.Exists(aoepath + "\\EMPIRESX.EXE"))
+            {
+                ttf += 1;
+            }
+            else
+            {
+                MessageBox.Show("Không phát hiện file game: 'EMPIRESX.EXE', vui lòng thử lại");
+            }
+            if (ttf == 2)
+            {
+                patchaoe();
+            }    
+        }
+        void patchaoe()
+        {
+            patchaoed();
+            patchaoex();
+            MessageBox.Show("Đã patch thành công AOE!!!");
+        }
+        void patchaoed()
+        {
+            using (var stream = new FileStream(aoepath + "\\EMPIRES.EXE", FileMode.Open, FileAccess.ReadWrite))
+            {
+                stream.Position = 951407;
+                stream.WriteByte(0x33);
+                stream.Position = 951408;
+                stream.WriteByte(0xC0);
+                stream.Position = 951409;
+                stream.WriteByte(0x40);
+                stream.Position = 951410;
+                stream.WriteByte(0x90);
+            }
+            patched += 1;
+        }
+        void patchaoex()
+        {
+            using (var stream = new FileStream(aoepath + "\\EMPIRESX.EXE", FileMode.Open, FileAccess.ReadWrite))
+            {
+                stream.Position = 102544;
+                stream.WriteByte(0x90);
+                stream.Position = 102545;
+                stream.WriteByte(0x90);
+            }
+            patched += 1;
+        }
+
+        private void Estbt_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
